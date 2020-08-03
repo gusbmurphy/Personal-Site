fe85d29e295e75c0ac856cb08c9b12938279cca2
Gus Murphy <50927042+gusbmurphy@users.noreply.github.com>
2020-08-03 09:44:00 -0500

    Update styling on work grid item

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,53 +6,62 @@ import { Link, useStaticQuery } from "gatsby"
 import { graphql } from "gatsby"
 
 const WorkItem = ({ post }) => {
-  let tags = post.frontmatter.tags.map(tag => (
-    <li className={styles.tag} key={tag}>
-      {tag}
+  let tags = ""
+  post.frontmatter.tags.forEach((tag, index) => {
+    tags += tag
+    if (index != (post.frontmatter.tags.length - 1)) tags += " / "
+  })
+
+  let links = post.frontmatter.links.map(link => (
+    <li>
+      <a target="_blank" rel="noopener noreferrer" href={link.url}>{link.display}</a>
     </li>
   ))
+
   let image = post.frontmatter.previewImage
   return (
     <div className={styles.workGridItem}>
       <Link to={post.fields.slug} className={styles.workGridItemImg}>
-      <img src={image} />
+        <img src={image} />
       </Link>
       <div className={styles.workGridItemInfo}>
-        <div className={styles.workTitle}>{post.frontmatter.title}</div>
+        <h3>{post.frontmatter.title}</h3>
+        <ul className={styles.tagsCollection}>{tags}</ul>
+        {post.frontmatter.date}
+        <br></br>
+        <ul className={styles.linksCollection}>{links}</ul>
         <br></br>
         <div className={styles.workGridItemDesc}>
           {post.frontmatter.shortDesc}
         </div>
       </div>
-      <div className={styles.workGridItemEnd}>
-        {post.frontmatter.date}
+      {/* <div className={styles.workGridItemEnd}>
         <br></br>
-        <ul className={styles.tagsCollection}>{tags}</ul>
-      </div>
+      </div> */}
     </div>
   )
 }
 
-const MobileWorkItem = ({ post }) => {
-  let tags = post.frontmatter.tags.map(tag => (
-    <li className={styles.tag} key={tag}>
-      {tag}
-    </li>
-  ))
-  return (
-    <div className={styles.workGridItem}>
-      <Link to={post.fields.slug} className={styles.workGridItemImg}>
-        <img src={post.frontmatter.previewImage} />
-      </Link>
-      <div className={styles.workGridItemInfo}>
-        <h2>{post.frontmatter.title}</h2>
-        {post.frontmatter.date}
-        <br></br>
-        <ul className={styles.tagsCollection}>{tags}</ul>
-      </div>
-    </div>
-  )
-}
+// const MobileWorkItem = ({ post }) => {
+//   let tags = post.frontmatter.tags.map(tag => (
+//     <li className={styles.tag} key={tag}>
+//       {tag}
+//     </li>
+//   ))
+//   return (
+//     <div className={styles.workGridItem}>
+//       <Link to={post.fields.slug} className={styles.workGridItemImg}>
+//         <img src={post.frontmatter.previewImage} />
+//       </Link>
+//       <div className={styles.workGridItemInfo}>
+//         <h2>{post.frontmatter.title}</h2>
+//         {post.frontmatter.date}
+//         <br></br>
+//         <ul className={styles.tagsCollection}>{tags}</ul>
+//       </div>
+//     </div>
+//   )
+// }
 
 export default ({
   data: {
@@ -60,20 +69,22 @@ export default ({
   },
 }) => {
   const works = allMarkdown.map(markdown => (
-    <Media
-      queries={{
-        mobile: "(max-width: 499px)",
-      }}
-    >
-      {matches => (
-        <Fragment>
-          {matches.mobile && <MobileWorkItem key={markdown.node.id} post={markdown.node} />}
-          {!matches.mobile && (
+    // <Media
+    //   queries={{
+    //     mobile: "(max-width: 499px)",
+    //   }}
+    // >
+    //   {matches => (
+    //     <Fragment>
+    //       {matches.mobile && (
+    //         <MobileWorkItem key={markdown.node.id} post={markdown.node} />
+    //       )}
+    //       {!matches.mobile && (
             <WorkItem key={markdown.node.id} post={markdown.node} />
-          )}
-        </Fragment>
-      )}
-    </Media>
+    //       )}
+    //     </Fragment>
+    //   )}
+    // </Media>
   ))
   return (
     <div style={{ textAlign: `center` }}>
@@ -101,6 +112,10 @@ export const pageQuery = graphql`
             shortDesc
             tags
             previewImage
+            links {
+              display
+              url
+            }
           }
         }
       }
