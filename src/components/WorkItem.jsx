4a6be24c1460eb48e11411515714fa93aeda47cd
Augustus Murphy <gusbmurphy@gmail.com>
2021-03-18 21:28:47 -0400

    Move WorkItem component to own file

---
+++ src/components/WorkItem.jsx
@@ -0,0 +1,66 @@
+import React from "react"
+import { Devicon } from "./Devicon"
+import { FAIcon } from "./FAIcon"
+
+export const WorkItem = ({ post }) => {
+  let links = post.frontmatter.links.map(link => (
+    <li key={link.url}>
+      <a target="_blank" rel="noopener noreferrer" href={link.url}>
+        {link.display}
+      </a>
+    </li>
+  ))
+
+  let icons = []
+  if (post.frontmatter.devicons) {
+    post.frontmatter.devicons.map(devicon =>
+      icons.push(
+        <Devicon
+          name={devicon.name}
+          description={devicon.description}
+          id={devicon.name + post.id}
+        />
+      )
+    )
+  }
+  if (post.frontmatter.faIcons) {
+    post.frontmatter.faIcons.map(faIcon =>
+      icons.push(
+        <FAIcon
+          name={faIcon.name}
+          description={faIcon.description}
+          id={faIcon.name + post.id}
+        />
+      )
+    )
+  }
+
+  let image = post.frontmatter.previewImage
+  return (
+    <div className={styles.individualContainer}>
+      <h3>
+        <Link to={post.fields.slug}>{post.frontmatter.title}</Link>
+        {post.frontmatter.backBurner && (
+          <span className={styles.wipWarning}>
+            &nbsp;&#40;work in progress&#41;
+          </span>
+        )}
+      </h3>
+      <div className={styles.imageDescContainer}>
+        <div className={styles.imageContainer}>
+          <Link to={post.fields.slug}>
+            <img src={image} />
+          </Link>
+        </div>
+        <div className={styles.descriptionContainer}>
+          <div className={styles.icons}>{icons}</div>
+          {post.frontmatter.date}
+          <br></br>
+          <ul className={styles.links}>{links}</ul>
+          <br></br>
+          {post.frontmatter.shortDesc}
+        </div>
+      </div>
+    </div>
+  )
+}
--- src/pages/work.js
+++ src/pages/work.js
@@ -1,89 +1,12 @@
 import React from "react"
 import * as styles from "../styles/work.module.css"
-import { Link } from "gatsby"
-import { graphql } from "gatsby"
-import { Devicon } from "../components/Devicon"
-import { FAIcon } from "../components/FAIcon"
-
-const WorkItem = ({ post }) => {
-  console.log(post)
-  // let tags = ""
-  // post.frontmatter.tags.forEach((tag, index) => {
-  //   tags += tag
-  //   if (index != post.frontmatter.tags.length - 1) tags += " / "
-  // })
-
-  let links = post.frontmatter.links.map(link => (
-    <li key={link.url}>
-      <a target="_blank" rel="noopener noreferrer" href={link.url}>
-        {link.display}
-      </a>
-    </li>
-  ))
-
-  let icons = []
-  if (post.frontmatter.devicons) {
-    post.frontmatter.devicons.map(devicon =>
-      icons.push(
-        <Devicon
-          name={devicon.name}
-          description={devicon.description}
-          id={devicon.name + post.id}
-        />
-      )
-    )
-  }
-  if (post.frontmatter.faIcons) {
-    post.frontmatter.faIcons.map(faIcon =>
-      icons.push(
-        <FAIcon
-          name={faIcon.name}
-          description={faIcon.description}
-          id={faIcon.name + post.id}
-        />
-      )
-    )
-  }
-
-  console.log("icons: ", icons)
-
-  let image = post.frontmatter.previewImage
-  return (
-    <div className={styles.individualContainer}>
-      <h3>
-        <Link to={post.fields.slug}>{post.frontmatter.title}</Link>
-        {post.frontmatter.backBurner && (
-          <span className={styles.wipWarning}>
-            &nbsp;&#40;work in progress&#41;
-          </span>
-        )}
-      </h3>
-      <div className={styles.imageDescContainer}>
-        <div className={styles.imageContainer}>
-          <Link to={post.fields.slug}>
-            <img src={image} />
-          </Link>
-        </div>
-        <div className={styles.descriptionContainer}>
-          <div className={styles.icons}>{icons}</div>
-          {post.frontmatter.date}
-          <br></br>
-          <ul className={styles.links}>{links}</ul>
-          <br></br>
-          {post.frontmatter.shortDesc}
-        </div>
-      </div>
-    </div>
-  )
-}
+import { WorkItem } from "../components/WorkItem"
 
 const WorkPage = ({
   data: {
     allMarkdownRemark: { edges: allMarkdown },
   },
 }) => {
-  console.log(allMarkdown)
-  console.log('hello!')
   let works = []
   let wips = []
   allMarkdown.forEach(markdown => {
